4c1631c6c8b7486756895a0370cf2685fbe92b85
老板 <rouban@roubantekimacbook-pro.local>
2016-07-04 14:34:33 +0800

    Initial Commit

---
+++ PrintFont/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrintFont
+//
+//  Created by 老板 on 16/7/4.
+//  Copyright © 2016年 老板. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
